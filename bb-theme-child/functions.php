086e10c7fee08392ec0044ba48cc46e6a7758227
Mitchell Sholly <contact@mitchellsholly.com>
2018-11-29 19:24:19 -0800

    Add search BB functions to navigation, set ticket default to 1 ticket

--- bb-theme-child/functions.php
+++ bb-theme-child/functions.php
@@ -1,11 +1,37 @@
 <?php
 
 // Defines
-define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
-define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+define('FL_CHILD_THEME_DIR', get_stylesheet_directory());
+define('FL_CHILD_THEME_URL', get_stylesheet_directory_uri());
 
 // Classes
 require_once 'classes/class-fl-child-theme.php';
 
 // Actions
-add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+add_action('wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000);
+
+/**
+ * Adding search icon at right side of the menu module
+ *
+ * @return void
+ */
+add_action('fl_builder_after_render_module', 'frame_add_search_icon_themer_header', 10);
+function frame_add_search_icon_themer_header($module)
+{
+    global $woocommerce;
+    //* Checking that you are using BB Theme
+    if (! class_exists('FLTheme')) {
+        return;
+    }
+
+    $id = '';
+
+    if ($module->settings->class === "add-search-icon") {
+        $id = $module->node;
+    }
+    if ($module->settings->type == "menu" && $module->node == $id) {
+        //* Displaying the search icon
+        FLTheme::nav_search();
+    }
+    return;
+}
