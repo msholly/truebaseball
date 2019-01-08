4d1aff9b2b5bf2f4461b0ec5f5b482283ff6b9a3
Mitchell Sholly <contact@mitchellsholly.com>
2019-01-08 13:57:07 -0800

    easier CSS approach instead

--- bb-theme-child/assets/js/custom.js
+++ bb-theme-child/assets/js/custom.js
@@ -1,7 +1,3 @@
 "use strict";
 
-jQuery(document).ready(function ($) {
-  $(".wpmenucart-icon-shopping-cart-0").load(function () {
-    $(".wpmenucart-icon-shopping-cart-0").addClass("fas fa-shopping-cart");
-  });
-});
+jQuery(document).ready(function ($) {});
--- bb-theme-child/assets/js/custom/custom.js
+++ bb-theme-child/assets/js/custom/custom.js
@@ -1,5 +1,2 @@
 jQuery(document).ready(function($) {
-	$(".wpmenucart-icon-shopping-cart-0").load(function() {
-		$(".wpmenucart-icon-shopping-cart-0").addClass("fas fa-shopping-cart");
-	});
 });
