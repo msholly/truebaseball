fc22dcc5cfb88b673492770e5d9c9081ac501b9b
Mitchell Sholly <contact@mitchellsholly.com>
2019-01-08 13:49:44 -0800

    Fix build

--- bb-theme-child/assets/js/custom.js
+++ bb-theme-child/assets/js/custom.js
@@ -1,15 +1,5 @@
 "use strict";
 
-/**
- * Custom JavaScript
- *
- * @since 1.0.0
- */
-var sub = function sub(a, b) {
-  return a - b;
-};
-"use strict";
-
 jQuery(document).ready(function ($) {
   $(".wpmenucart-icon-shopping-cart-0").load(function () {
     $(".wpmenucart-icon-shopping-cart-0").addClass("fas fa-shopping-cart");
