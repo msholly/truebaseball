6542ed00d4b6e301fcf4069bbba2f5764a3c0185
Mitchell Sholly <contact@mitchellsholly.com>
2019-01-10 12:34:24 -0800

    Add search and logo to side mobile menu

--- bb-theme-child/functions.php
+++ bb-theme-child/functions.php
@@ -47,9 +47,25 @@ function my_custom_fonts() {
   </style>';
 }
 
+
+// THE WEIRD BUG LOADING ADMIN COMMON ON THE FRONT END
 function project_dequeue_unnecessary_styles() {
-  // THE WEIRD BUG LOADING ADMIN COMMON ON THE FRONT END
   wp_dequeue_style( 'common' );
   wp_deregister_style( 'common' );
 }
 if ( ! is_admin() ) add_action( 'wp_enqueue_scripts', 'project_dequeue_unnecessary_styles' );
+
+
+// Add a Beaver Builder Template to the bottom of the Beaver Builder Theme vertical menu
+add_filter( 'wp_nav_menu_items', 'your_custom_menu_item', 10, 2 );
+function your_custom_menu_item ( $items, $args ) {
+    // if the menu is in fact our mobile main menu
+    if ($args->menu == 'mobile-main-menu') {
+        // get the content of our Beaver Builder Template
+    	$bb_content = do_shortcode('[fl_builder_insert_layout slug="vertical-menu-content-bottom"]');
+    	// append the content of our Beaver Builder Template to the beginning of our menu
+        $items = $bb_content . $items;
+    }
+    // return our menu + Beaver Builder content ready for output
+    return $items;
+}
