e0833ba2f71450fe2590741f1d20d2156f840203
Mitchell Sholly <contact@mitchellsholly.com>
2019-07-09 16:10:51 -0700

    new user roles, and set affiliate on signup

---
+++ true-functions/true-functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: TRUE Functions
+ * Plugin URI: https://architkmedia.com
+ * Description: Additional Functions for TRUE
+ * Version: 1.0.0
+ * Author: Mitchell Sholly
+ * Author URI: http://mitchellsholly.com
+ * License: GPL2
+ */
+
+add_role(
+    'affiliate',
+    __( 'Affiliate' ),
+    array(
+        'read'         => true,  // true allows this capability
+    )
+);
+
+add_role(
+    'sales_rep',
+    __( 'Sales Rep' ),
+    array(
+        'read'         => true,  // true allows this capability
+    )
+);
+
+function pw_affwp_set_role_on_registration( $affiliate_id = 0 ) {
+	$user_id = affwp_get_affiliate_user_id( $affiliate_id );
+	$user = new WP_User( $user_id );
+	$user->add_role( 'affiliate' );
+}
+add_action( 'affwp_insert_affiliate', 'pw_affwp_set_role_on_registration' );
+
+
+
+?>
