3533f53191cc23ddd6713ded92f8936918dd416e
Mitchell Sholly <contact@mitchellsholly.com>
2019-07-12 16:43:56 -0700

    add transaction_id and true meta fields to legacy API endpoint

--- true-functions/true-functions.php
+++ true-functions/true-functions.php
@@ -33,5 +33,30 @@ function pw_affwp_set_role_on_registration( $affiliate_id = 0 ) {
 add_action( 'affwp_insert_affiliate', 'pw_affwp_set_role_on_registration' );
 
 
+// For ACF Debuggin
+// add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
+
+
+/*
+ * Legacy API
+ * Add a referanse field to the Order API response.
+*/ 
+function prefix_wc_api_order_response( $order ) {
+    // Get the value
+    $transaction_id = get_post_meta($order['id'], '_transaction_id', true);
+
+	$true_meta_event = ( $value = get_field('order_type', $order['id']) ) ? $value : '';
+	$true_meta_salesrep = ( $value = get_field('sales_rep', $order['id']) ) ? $value : '';
+	$true_meta_affiliate = ( $value = get_field('affiliate', $order['id']) ) ? $value : '';
+
+	$order['true_meta_event'] = $true_meta_event;
+	$order['true_meta_salesrep'] = $true_meta_salesrep;
+	$order['true_meta_affiliate'] = $true_meta_affiliate;
+	$order['transaction_id'] = $transaction_id;
+
+	return $order;
+}
+add_filter( 'woocommerce_api_order_response', 'prefix_wc_api_order_response', 10, 1 );
+
 
 ?>
