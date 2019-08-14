707069fdc2d26e9fe45b787957f734b7ea5d7817
Mitchell Sholly <contact@mitchellsholly.com>
2019-08-14 16:18:56 -0700

    auto add tickets to a woo category, so affiliate rates are set automatically based on category

--- true-functions/true-functions.php
+++ true-functions/true-functions.php
@@ -40,6 +40,17 @@ add_action( 'affwp_insert_affiliate', 'pw_affwp_set_role_on_registration' );
 // For ACF Debuggin
 add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
 
+// Add a Woo Product Category on ticket save
+add_action( 'event_tickets_after_save_ticket', 'tribe_events_add_product_category_to_tickets', 10, 4 );
+
+function tribe_events_add_product_category_to_tickets( $event_id, $ticket, $raw_data, $classname ) {
+
+    if ( ! empty( $ticket ) && isset( $ticket->ID ) ) {
+        wp_add_object_terms( $ticket->ID, 'tickets', 'product_cat' );
+    }
+
+}
+
 
 /*
  * Legacy API
