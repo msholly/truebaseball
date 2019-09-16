1a5328abac72673fe9e3b1745e994bd44e68b6a8
Mitchell Sholly <contact@mitchellsholly.com>
2019-09-16 13:10:04 -0700

    Event organizer notification code

--- true-functions/php/woocommerce/index.php
+++ true-functions/php/woocommerce/index.php
@@ -223,3 +223,24 @@ add_filter( 'cfw_get_shipping_checkout_fields', 'true_unset_shipping_when_free_i
  * Hides the product's weight and dimension in the single product page.
  */
 add_filter( 'wc_product_enable_dimensions_display', '__return_false' );
+
+
+/**
+ *  Add a custom email to the list of emails WooCommerce should load
+ *
+ * @since 0.1
+ * @param array $email_classes available email classes
+ * @return array filtered available email classes
+ */
+function add_event_order_woocommerce_email( $email_classes ) {
+
+	// include our custom email class
+	require_once( 'class-wc-event-organizer-order-email.php' );
+
+	// add the email class to the list of email classes that WooCommerce loads
+	$email_classes['WC_Event_Organizer_Order_Email'] = new WC_Event_Organizer_Order_Email();
+
+	return $email_classes;
+
+}
+add_filter( 'woocommerce_email_classes', 'add_event_order_woocommerce_email' );
