b5efa1b3985650c214af0f049f397df8823cd322
Mitchell Sholly <contact@mitchellsholly.com>
2019-11-21 14:19:58 -0800

    Automated JustBats URL building

--- true-functions/php/fitting/justBats.php
+++ true-functions/php/fitting/justBats.php
@@ -24,7 +24,28 @@ function true_get_just_bats_url($productId)
                 // Remove Starting slash just in case
                 $slug = substr($slug, 1);
             }
-            $full_justbats_url = $base_justbats_url . $slug;
+
+            // get variant info
+            $product = wc_get_product($productId);
+            $bat_variant = $product->get_variation_attributes();
+            $bat_grip = $bat_variant['attribute_pa_grip-size'];
+            $bat_length = $bat_variant['attribute_pa_length-in'];
+
+            // Replace to match Justbats
+            if ($bat_grip == 'youth-standard') {
+                $bat_grip = 'Small%20Grip';
+            }
+            if ($bat_grip == 'youth-oversized') {
+                $bat_grip = 'Med%20Grip';
+            }
+
+            // slug to back decimal
+            $bat_length = str_replace('-', '.', $bat_length);
+
+            // FINAL target
+            // https://www.justbats.com/product/true-t1--10-usa-baseball-bat--yb-t1-20-10/32218/?attr=28.5%22|Small%20Grip
+            $full_justbats_url = $base_justbats_url . $slug . '/?attr=' . $bat_length . '%22|' . $bat_grip;
+
         }
         
     }
