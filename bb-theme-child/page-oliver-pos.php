6fddec5dccc2957e10bc6e334852bd7861a32b7b
mitchell_sholly <contact@mitchellsholly.com>
2020-01-24 15:40:07 -0800

    Temporary tax extension disable

--- bb-theme-child/page-oliver-pos.php
+++ bb-theme-child/page-oliver-pos.php
@@ -773,7 +773,7 @@
                     console.log(taxjsonMsg);
                     console.log("^^ TAX DATA TO OLIVER EXTENSION ^^")
 
-                    sendMessage(JSON.stringify(taxjsonMsg));
+                    // sendMessage(JSON.stringify(taxjsonMsg));
 
                     // MESSAGES SENT TO OLIVER, ALLOW FINISH EXTENSION BUTTON
                     $(this).text("TAGS SAVED");
