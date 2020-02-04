cf7cb315c1695fea4e59a17931d3f3bcbf97b317
mitchell_sholly <contact@mitchellsholly.com>
2020-02-04 11:13:00 -0800

    Re-enable Oliver taxes

--- bb-theme-child/page-oliver-pos.php
+++ bb-theme-child/page-oliver-pos.php
@@ -776,9 +776,9 @@
                         }
                     }
                     console.log(taxjsonMsg);
-                    console.log("^^ NO TAX DATA TO OLIVER EXTENSION ^^")
+                    console.log("^^ TAX DATA TO OLIVER EXTENSION ^^")
 
-                    // sendMessage(JSON.stringify(taxjsonMsg));
+                    sendMessage(JSON.stringify(taxjsonMsg));
 
                     // MESSAGES SENT TO OLIVER, ALLOW FINISH EXTENSION BUTTON
                     $(this).text("TAGS SAVED");
